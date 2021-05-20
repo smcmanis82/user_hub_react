17ca8334ee99718741e7cca5fde9d9af629ad757
Sharon McManis <sharon.mcmanis@gmail.com>
2021-05-20 14:59:33 -0500

    setup again WITH src

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div id="App">
+      <h1>Hello, World</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
